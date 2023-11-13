82f6d08a44d2a9e60add5bac9f1a09e420fe30d6
Griffin Scott <griffinoscott@gmail.com>
2023-11-12 21:18:20 -0800

    implementing some db stuff

--- api/app.js
+++ api/app.js
@@ -20,4 +20,5 @@ const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
+    console.log(`node_env: ${process.env.NODE_ENV}`)
 });
--- api/routes.js
+++ api/routes.js
@@ -1,4 +1,7 @@
+const mongoose = require("mongoose")
+
 const pkmnJson = require('./data/pokemon-details.json');
+
 
 let express = require("express"),
 router = express.Router();
@@ -12,38 +15,12 @@ router.get("/status", (request, response) => {
 });
 
 router.get("/pokemon", async (request, response) => {
-    // const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1292')
-    // const data = await res.json()
     const pokemon = pkmnJson
     response.send(pokemon)
 });
 
 router.get("/pokemon/:id", async (request, response) => {
-    // const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${request.params.id}`)
-    // const data = await res.json()
     const data = pkmnJson[request.params.id]
-
-    // const pokeTypes = []
-    // const pokeStats = []
-
-    // for (t of data.types){
-    //     pokeTypes.push(t.type.name)
-    // }
-
-    // for (s of data.stats){
-    //     pokeStats.push(s.base_stat)
-    // }
-
-    // const pokemon = {
-    //     id: data.id,
-    //     name: data.name,
-    //     image: data.sprites.front_default,
-    //     species: data.species,
-    //     height: data.height,
-    //     weight: data.weight,
-    //     stats: pokeStats,
-    //     types: pokeTypes
-    // }
 
     response.send(JSON.stringify(data))
 });
