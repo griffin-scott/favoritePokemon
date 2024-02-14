d4060e3cb79bff6c8ef1528acae382ecdf226320
Griffin Scott <griffinoscott@gmail.com>
2024-02-14 05:36:14 -0800

    .

---
+++ api/models/PokemonModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const pokemonSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        // required: true,
+    },
+    name: {
+        type: String,
+        // required: true,
+    },
+    types: {
+        type: Array,
+        default: [],
+        // required: true,
+    },
+    abilities: {
+        type: Array,
+        default: [],
+        // required: true,
+    },
+    base_experience: {
+        type: Number,
+    },
+    species: {
+        name: {
+            type: String,
+            // required: true,
+        },
+        url: {
+            type: String,
+            // required: true,
+        },
+    },
+    sprites: {
+        type: Array,
+        default: [],
+        // required: true,
+    },
+    stats: {
+        type: Array,
+        default: [],
+        // required: true,
+    },
+    height: {
+        type: Number,
+        // required: true,
+    },
+    weight: {
+        type: Number,
+        // required: true,
+    },
+}, {collection: "pokemon"});
+
+module.exports = mongoose.model("Pokemon", pokemonSchema)
